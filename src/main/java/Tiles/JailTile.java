33f69c6bdeff9e9b099741da003cd2d21d93a4c0
MaxLyu <113391218+Maxlyu254@users.noreply.github.com>
2022-11-07 23:14:41 -0500

    Added most entities and methods required for the basic use cases. Still more methods and documentations needed.

---
+++ src/main/java/Tiles/JailTile.java
@@ -0,0 +1,9 @@
+package Tiles;
+
+/**
+ * JailTile is the tile where the player goes to when it steps on "go to jail". It does nothing to the player that
+ * stepped on it.
+ */
+public class JailTile extends Tile {
+
+}
