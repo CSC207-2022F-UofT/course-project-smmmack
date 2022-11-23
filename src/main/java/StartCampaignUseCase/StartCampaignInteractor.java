efbde869e85fab1243f92032206cce6a3186cbdc
MaxLyu <113391218+Maxlyu254@users.noreply.github.com>
2022-11-23 11:42:17 -0500

    partly finished the Controller, Presenter, and Interactor

--- src/main/java/StartCampaignUseCase/StartCampaignInteractor.java
+++ src/main/java/StartCampaignUseCase/StartCampaignInteractor.java
@@ -5,6 +5,12 @@ import MainEntities.Campaign;
 public class StartCampaignInteractor implements StartCampaignInputBoundary {
 
     private Campaign campaign;
+    private StartCampaignOutputBoundary outputBoundary;
+
+    public StartCampaignInteractor(Campaign campaign, StartCampaignOutputBoundary outputBoundary) {
+        this.campaign = campaign;
+        this.outputBoundary = outputBoundary;
+    }
 
     @Override
     public void performAction(StartCampaignInputData inputData) {
@@ -17,9 +23,17 @@ public class StartCampaignInteractor implements StartCampaignInputBoundary {
         return campaign;
     }
 
+    public StartCampaignOutputBoundary getOutputBoundary() {
+        return outputBoundary;
+    }
+
     //setters
 
     public void setCampaign(Campaign campaign) {
         this.campaign = campaign;
+    }
+
+    public void setOutputBoundary(StartCampaignOutputBoundary outputBoundary) {
+        this.outputBoundary = outputBoundary;
     }
 }
--- src/main/java/StartCampaignUseCase/StartCampaignPresenter.java
+++ src/main/java/StartCampaignUseCase/StartCampaignPresenter.java
@@ -1,4 +1,9 @@
 package StartCampaignUseCase;
 
-public class StartCampaignPresenter {
+public class StartCampaignPresenter implements StartCampaignOutputBoundary {
+
+    @Override
+    public void performAction(StartCampaignOutputData outputData) {
+
+    }
 }
