a1437f9b0fac376ac4ddfd65f56927239da666de
MaxLyu <113391218+Maxlyu254@users.noreply.github.com>
2022-11-25 14:00:33 -0500

    Implemented most fundamental functions of the View Models

--- src/main/java/ViewModel/PlayerPanelViewModel.java
+++ src/main/java/ViewModel/PlayerPanelViewModel.java
@@ -1,4 +1,53 @@
 package ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Currently simply a list of player VMs. more contents may be added later.
+ */
 public class PlayerPanelViewModel {
+    private List<PlayerViewModel> playerVMs;
+    private List<PlayerPanelVMListener> listeners;
+
+    public PlayerPanelViewModel(List<PlayerViewModel> playerVMs) {
+        this.playerVMs = playerVMs;
+        this.listeners = new ArrayList<>();
+    }
+
+    //setters
+
+    public void setPlayerVMs(List<PlayerViewModel> playerVMs) {
+        this.playerVMs = playerVMs;
+    }
+
+    //getters
+
+    public List<PlayerViewModel> getPlayerVMs() {
+        return playerVMs;
+    }
+
+    //other getters
+
+    public PlayerViewModel getPlayerVMAt(int index) {
+        return this.playerVMs.get(index);
+    }
+
+    //other setters
+
+    public void addListener(PlayerPanelVMListener listener) {
+        this.listeners.add(listener);
+    }
+
+    public boolean removeListener(PlayerPanelVMListener listener) {
+        return this.listeners.remove(listener);
+    }
+
+    //other methods
+
+    public void notifyListeners() {
+        for (PlayerPanelVMListener listener: listeners) {
+            listener.performAction();
+        }
+    }
 }
