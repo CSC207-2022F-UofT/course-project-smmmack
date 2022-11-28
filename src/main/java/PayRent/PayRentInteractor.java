966d6d46b8c9b2a6bab57ad6622499868ff273b7
adelinewang3 <adeline.wang@mail.utoronto.ca>
2022-11-28 17:47:57 -0500

    Wrote an if else statement so code would terminate - in the performAction method of the PayRentInteractor class

--- src/main/java/PayRent/PayRentInteractor.java
+++ src/main/java/PayRent/PayRentInteractor.java
@@ -19,27 +19,25 @@ public class PayRentInteractor implements PayRentInputBoundary{
             String outputMessage = "Player does not have enough money to pay the rent.";
             PayRentOutputData payRentOutputData = new PayRentOutputData(outputMessage);
             payRentOutputBoundary.payRentMessage(payRentOutputData);
-        }
-
-        if(payRentInputData.getIsMortgaged()){
+        } else if(payRentInputData.getIsMortgaged()){
             String outputMessage = "No rent is paid as this property is mortgaged.";
             PayRentOutputData payRentOutputData = new PayRentOutputData(outputMessage);
             payRentOutputBoundary.payRentMessage(payRentOutputData);
+        } else {
+            // may need to change this part based on how getRent() is implemented
+            int rentMoneyPaid = payRentInputData.getRentee().getCash() -
+                    payRentInputData.getPropertyLandedOn().getRent(payRentInputData.getRentee());
+
+            payRentInputData.getRentee().setCash(rentMoneyPaid);
+            payRentInputData.setRentMoney(rentMoneyPaid);
+
+            String outputMessage = payRentInputData.getRentee() + " paid " +
+                    payRentInputData.getRentMoney() + "of rent money to " + payRentInputData.getRenter();
+
+            PayRentOutputData payRentOutputData = new PayRentOutputData(outputMessage);
+
+            payRentOutputBoundary.payRentMessage(payRentOutputData);
         }
-
-        // may need to change this part based on how getRent() is implemented
-        int rentMoneyPaid = payRentInputData.getRentee().getCash() -
-                payRentInputData.getPropertyLandedOn().getRent(payRentInputData.getRentee());
-
-        payRentInputData.getRentee().setCash(rentMoneyPaid);
-        payRentInputData.setRentMoney(rentMoneyPaid);
-
-        String outputMessage = payRentInputData.getRentee() + " paid " +
-                payRentInputData.getRentMoney() + "of rent money to " + payRentInputData.getRenter();
-
-        PayRentOutputData payRentOutputData = new PayRentOutputData(outputMessage);
-
-        payRentOutputBoundary.payRentMessage(payRentOutputData);
 
     }
 
