662002ba96e909994db6cde2a1c53ed9a956b5d8
adelinewang3 <adeline.wang@mail.utoronto.ca>
2022-11-30 12:15:27 -0500

    Updated payRentMessage in PayRentPresenter - now when payRentMessage is called, the PlayerViewModel and CommandLineViewModel will be updated

--- src/main/java/PayRent/PayRentPresenter.java
+++ src/main/java/PayRent/PayRentPresenter.java
@@ -1,12 +1,40 @@
 package PayRent;
 
+import ViewModel.CommandLineViewModel;
+import ViewModel.PlayerViewModel;
+
 public class PayRentPresenter implements PayRentOutputBoundary{
+
+    private PlayerViewModel renteePlayerViewModel;
+    private PlayerViewModel renterPlayerViewModel;
+    private CommandLineViewModel commandLineViewModel;
+
+    public PayRentPresenter(PlayerViewModel renteePlayerViewModel, PlayerViewModel renterPlayerViewModel,
+                            CommandLineViewModel commandLineViewModel){
+
+        this.renteePlayerViewModel = renteePlayerViewModel;
+        this.renterPlayerViewModel = renterPlayerViewModel;
+        this.commandLineViewModel = commandLineViewModel;
+
+    }
 
 
     @Override
-    public void payRentMessage(PayRentOutputData payRentOutputData) {
+    public void payRentMessage(boolean paidRent, int renteeCash, int renterCash, PayRentOutputData payRentOutputData) {
+
         String message = payRentOutputData.getOutputMessage();
-        // Will update the view model to add the output message to the command lines later
+
+        if (paidRent){
+            renteePlayerViewModel.setCash(renteeCash);
+            renterPlayerViewModel.setCash(renterCash);
+            commandLineViewModel.setMessage(message);
+            commandLineViewModel.setType("output");
+        } else{
+            commandLineViewModel.setMessage(message);
+            commandLineViewModel.setType("output");
+        }
+
     }
+
 
 }
