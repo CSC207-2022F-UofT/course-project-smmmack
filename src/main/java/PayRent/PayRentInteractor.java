67cb25931e62cd743313e14311793dd22d9c35fe
adelinewang3 <adeline.wang@mail.utoronto.ca>
2022-12-01 15:45:35 -0500

    Updated performAction method - added in the case where the rentee had to manually confirm pay rent

--- src/main/java/PayRent/PayRentInteractor.java
+++ src/main/java/PayRent/PayRentInteractor.java
@@ -1,11 +1,28 @@
 package PayRent;
 
+import MainEntities.Player;
+import Properties.Property;
+
 public class PayRentInteractor implements PayRentInputBoundary{
 
     private PayRentOutputBoundary payRentOutputBoundary;
+    private final Player rentee;
+    private final Player renter;
+    private final Property propertyLandedOn;
 
-    public PayRentInteractor(PayRentOutputBoundary payRentOutputBoundary) {
+    /**
+     * Constructor for PayRentInteractor
+     * @param payRentOutputBoundary output boundary
+     * @param rentee the player who lands on a property
+     * @param renter the player who owes the propety rentee landed on
+     * @param propertyLandedOn the property that rentee landed on
+     */
+    public PayRentInteractor(PayRentOutputBoundary payRentOutputBoundary, Player rentee, Player renter,
+                             Property propertyLandedOn) {
         this.payRentOutputBoundary = payRentOutputBoundary;
+        this.rentee = rentee;
+        this.renter = renter;
+        this.propertyLandedOn = propertyLandedOn;
     }
 
     /**
@@ -15,28 +32,51 @@ public class PayRentInteractor implements PayRentInputBoundary{
     @Override
     public void performAction(PayRentInputData payRentInputData) {
 
-        if (payRentInputData.getRentee().getCash() < payRentInputData.getRentMoney()){
-            String outputMessage = "Player does not have enough money to pay the rent.";
+        // set the input data
+        payRentInputData.setRentee(rentee.getName());
+        payRentInputData.setRenteeCash(rentee.getCash());
+        payRentInputData.setRenter(renter.getName());
+        payRentInputData.setRenterCash(renter.getCash());
+        payRentInputData.setPropertyLandedOn(propertyLandedOn.getAbbreviation());
+        payRentInputData.setRentMoney(propertyLandedOn.getRent(rentee));
+        payRentInputData.setIsMortgaged(propertyLandedOn.isMortgaged());
+
+        if(!payRentInputData.getConfirmPayRent()){
+            String outputMessage = "Please make sure you have enough cash before you confirm to pay rent. " +
+                    "You can sell your asses to get more cash or declare bankruptcy.";
             PayRentOutputData payRentOutputData = new PayRentOutputData(outputMessage);
-            payRentOutputBoundary.payRentMessage(payRentOutputData);
-        } else if(payRentInputData.getIsMortgaged()){
+            payRentOutputBoundary.payRentMessage(false, 0,0, payRentOutputData);
+        } else if(payRentInputData.getIsMortgaged()){ // if the property is mortgaged
             String outputMessage = "No rent is paid as this property is mortgaged.";
             PayRentOutputData payRentOutputData = new PayRentOutputData(outputMessage);
-            payRentOutputBoundary.payRentMessage(payRentOutputData);
-        } else {
-            // may need to change this part based on how getRent() is implemented
-            int rentMoneyPaid = payRentInputData.getRentee().getCash() -
-                    payRentInputData.getPropertyLandedOn().getRent(payRentInputData.getRentee());
+            payRentOutputBoundary.payRentMessage(false, 0,0, payRentOutputData);
+        } else if(payRentInputData.getRenteeCash() < payRentInputData.getRentMoney()){
+            // if the rentee does not have enough money to pay the rent
+            String outputMessage = "Player does not have enough money to pay the rent.";
+            PayRentOutputData payRentOutputData = new PayRentOutputData(outputMessage);
+            payRentOutputBoundary.payRentMessage(false, 0, 0, payRentOutputData);
+        } else if (payRentInputData.getRenteeCash() > payRentInputData.getRentMoney()){
+            // if the rentee has enough money to pay the rent
 
-            payRentInputData.getRentee().setCash(rentMoneyPaid);
-            payRentInputData.setRentMoney(rentMoneyPaid);
+            int rentMoney = propertyLandedOn.getRent(rentee);
+
+            rentee.loseCash(rentMoney);
+            payRentInputData.setRenteeCash(rentee.getCash());
+            renter.gainCash(rentMoney);
+            payRentInputData.setRenterCash(renter.getCash());
 
             String outputMessage = payRentInputData.getRentee() + " paid " +
                     payRentInputData.getRentMoney() + "of rent money to " + payRentInputData.getRenter();
 
             PayRentOutputData payRentOutputData = new PayRentOutputData(outputMessage);
 
-            payRentOutputBoundary.payRentMessage(payRentOutputData);
+            payRentOutputBoundary.payRentMessage(false, payRentInputData.getRenteeCash(),
+                    payRentInputData.getRenterCash(), payRentOutputData);
+
+        } else{ // if something else happens
+            String outputMessage = "Error. Please try again.";
+            PayRentOutputData payRentOutputData = new PayRentOutputData(outputMessage);
+            payRentOutputBoundary.payRentMessage(false, 0, 0, payRentOutputData);
         }
 
     }
