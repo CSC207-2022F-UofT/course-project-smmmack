925a59370bba68f29762ab78c84ea90ce4eb9732
adelinewang3 <adeline.wang@mail.utoronto.ca>
2022-12-01 14:44:22 -0500

    Added docstrings

--- src/main/java/PayRent/PayRentPresenter.java
+++ src/main/java/PayRent/PayRentPresenter.java
@@ -3,6 +3,9 @@ package PayRent;
 import ViewModel.CommandLineViewModel;
 import ViewModel.PlayerViewModel;
 
+/**
+ * This class updates the view models for the PlayerViewModel and CommandLineViewModel
+ */
 public class PayRentPresenter implements PayRentOutputBoundary{
 
     private PlayerViewModel renteePlayerViewModel;
@@ -18,7 +21,13 @@ public class PayRentPresenter implements PayRentOutputBoundary{
 
     }
 
-
+    /**
+     * Send a message to update the PlayerViewModel and CommandLineViewModel
+     * @param paidRent the amount of rent money the rentee paid
+     * @param renteeCash the amount of cash the rentee has
+     * @param renterCash the amount of cash the renter has
+     * @param payRentOutputData output data that is going to be displayed
+     */
     @Override
     public void payRentMessage(boolean paidRent, int renteeCash, int renterCash, PayRentOutputData payRentOutputData) {
 
