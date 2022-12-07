d9cf776441b7aaf1e6a957973c2860c3aa5824a9
MaxLyu <113391218+Maxlyu254@users.noreply.github.com>
2022-12-06 21:33:04 -0500

    Changed Controllers to accommodate the change in Exception throw in Command Performer

--- src/main/java/ReadCampaignUseCase/ReadCampaignController.java
+++ src/main/java/ReadCampaignUseCase/ReadCampaignController.java
@@ -1,5 +1,6 @@
 package ReadCampaignUseCase;
 
+import Exceptions.WrongCommandArgumentException;
 import UseCaseUniversal.CommandPerformer;
 
 import java.security.InvalidParameterException;
@@ -11,10 +12,16 @@ public class ReadCampaignController implements CommandPerformer {
         this.inputBoundary = inputBoundary;
     }
 
+    /**
+     * The command accepted by this controller should be in the following pattern: <br>
+     * read_campaign [file_name] <br>
+     * file name: the name of the save file.
+     * @param command the command string (user input)
+     */
     @Override
-    public void performCommand(String command) {
+    public void performCommand(String command) throws Exception {
         String[] words = command.split("\\s+");
-        if (words.length != 2) throw new InvalidParameterException("This command only takes 1 variable");
+        if (words.length != 2) throw new WrongCommandArgumentException("This command only takes 1 variable");
         ReadCampaignInputData inputData = new ReadCampaignInputData(words[1]);
         inputBoundary.performAction(inputData);
     }
--- src/main/java/StartDefCampUseCase/StartDefCampController.java
+++ src/main/java/StartDefCampUseCase/StartDefCampController.java
@@ -1,5 +1,6 @@
 package StartDefCampUseCase;
 
+import Exceptions.WrongCommandArgumentException;
 import UseCaseUniversal.CommandPerformer;
 
 public class StartDefCampController implements CommandPerformer {
@@ -9,12 +10,18 @@ public class StartDefCampController implements CommandPerformer {
         this.inputBoundary = inputBoundary;
     }
 
-    public void performCommand(String command) throws IllegalArgumentException{
+    /**
+     * The command accepted by this controller should be in the following pattern: <br>
+     * save_default_campaign [num] <br>
+     * num: the number of players
+     * @param command the command string (user input)
+     */
+    public void performCommand(String command) throws Exception {
         String[] words = command.split("\\s+");
         if (words.length != 2)
-            throw new IllegalArgumentException("This command only takes in one parameter");
+            throw new WrongCommandArgumentException("This command only takes in one parameter");
         if (!words[1].matches("\\d+"))
-            throw new IllegalArgumentException("The argument of this command may only be a non-negative integer");
+            throw new WrongCommandArgumentException("The argument of this command may only be a non-negative integer");
         int playerNum = Integer.parseInt(words[1]);
         StartDefCampInputData inputData = new StartDefCampInputData(playerNum);
         inputBoundary.performAction(inputData);
