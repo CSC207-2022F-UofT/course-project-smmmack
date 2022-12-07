84b5d878731e87d73109ec8d765e552cff194af3
MaxLyu <113391218+Maxlyu254@users.noreply.github.com>
2022-12-07 17:36:37 -0500

    Implemented end turn use case

---
+++ src/main/java/EndTurnUseCase/EndTurnContoller.java
@@ -0,0 +1,17 @@
+package EndTurnUseCase;
+
+import MainEntities.Exceptions.WrongCommandArgumentException;
+import UseCaseUniversal.CommandPerformer;
+
+public class EndTurnContoller implements CommandPerformer {
+    private EndTurnInputBoundary inputBoundary;
+
+    @Override
+    public void performCommand(String command) throws Exception {
+        String[] words = command.split("\\s+");
+        if (words.length != 1)
+            throw new WrongCommandArgumentException("This command takes in no argument");
+        inputBoundary.performAction(new EndTurnInputData());
+
+    }
+}
