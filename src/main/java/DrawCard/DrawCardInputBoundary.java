9e5d27da49eb904c4d29bb8b05babbeab36cc411
Maria Mustard <mariamustard21@gmail.com>
2022-12-08 16:27:28 -0500

    DrawCard use case Update 2

--- src/main/java/DrawCard/DrawCardInputBoundary.java
+++
@@ -1,7 +0,0 @@
-package DrawCard;
-
-import MainEntities.Deck;
-
-public interface DrawCardInputBoundary {
-    void performAction(DrawCardInputData drawCardInput, String deckType) throws Exception;
-}
